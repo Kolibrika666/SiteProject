aeb4ad95564ad6835571c6514697666abaeb6cfd
Kolibrika666 <kolibrika666@gmail.com>
2025-08-28 21:20:55 +0700

    init: footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,20 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
 function Footer() {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">ШЦТ</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Выполнено наставником: <a href="#login">Варкентин Юлия</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-
-    return (
-      <>
-        Footer
-      </>
-    )
-  }
-  
-  export default Footer
+export default Footer;
