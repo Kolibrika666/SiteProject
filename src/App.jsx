bb37b979ec429e99455aa412ea81c99f72d5c182
Kolibrika666 <kolibrika666@gmail.com>
2025-08-28 20:50:01 +0700

    init: container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { Container } from "react-bootstrap"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Home from "./pages/Home/Home"
 
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <Container>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </Container>
   )
 }
 
